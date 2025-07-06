31990802ea577792a50496b889be5026c0470259
aquapi <aquaplmc@gmail.com>
2025-07-06 17:02:24 +0700

    Add line numbers

--- comparisons/di/dependent-services/_.ts
+++ comparisons/di/dependent-services/_.ts
@@ -1,7 +1,7 @@
 import { readDir, test } from '../../utils.js';
 
 export default test({
-  title: 'Chained dependencies',
+  title: 'Dependent services',
   description:
     'Use and inject 3 dependencies, with `log` depends on `config` and `db` depends on both `log` and `config`.',
   code: readDir(import.meta.dir, ['udic', 'effect']),
