1321d8b2e42002093072dca0d0d45cae571ccd11
aquapi <aquaplmc@gmail.com>
2025-07-06 18:23:20 +0700

    Update

--- comparisons/di/dependent-services/udic.ts
+++ comparisons/di/dependent-services/udic.ts
@@ -4,41 +4,38 @@ const config = di.service('config')<{
   logLevel: string;
   connection: string;
 }>();
-
-const log = di.service('log')<(msg: string) => void>();
-
+const log = di.service('log')<
+  (msg: string) => void
+>();
 const db = di.service('db')<{
   query: (sql: string) => { result: string };
 }>();
 
 // Create service implementations that depends on other services
-const logLayer = di.layer(
-  log,
-  di.derive([config], ({ logLevel }) => (msg) => {
+const logImpl = di.impl(log, di.derive(
+  [config],
+  ({ logLevel }) => (msg) => {
     console.log(`[${logLevel}] ${msg}`);
-  }),
-);
-
-const dbLayer = di.layer(
-  db,
-  di.derive([config, log], ({ connection }, log) => ({
+  },
+));
+const dbImpl = di.impl(db, di.derive(
+  [config, log],
+  ({ connection }, log) => ({
     query: (sql: string) => {
       log('Executing query: ' + sql);
       return { result: 'Results from ' + connection };
     },
-  })),
-);
+  }),
+));
 
-// Provide implementations for the layer
-const logImpl = di.provide([logLayer], {
+// Provide dependencies for the impl
+const logCtx = di.merge([logImpl], {
   config: {
     logLevel: 'INFO',
     connection: 'mysql://username:password@hostname:port/database_name',
   },
 });
-const dbImpl = di.provide([dbLayer], logImpl);
+const dbCtx = di.merge([dbImpl], logCtx);
 
-const main = di.derive([db], (db) => {
-  console.log(db.query('SELECT * FROM users'));
-});
-main(dbImpl);
+const main = di.derive([db], (db) => db.query('SELECT * FROM users'));
+console.log(main(dbCtx));
