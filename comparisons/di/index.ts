0b7bb1a75b21dc8116f1c9188a8a5f9661efbd6e
aquapi <aquaplmc@gmail.com>
2025-07-09 00:08:32 +0700

    Update

--- comparisons/di/index.ts
+++ comparisons/di/index.ts
@@ -1,4 +1,5 @@
 import oneDep from './one-dependency/_.js';
+import multiDep from './multiple-services/_.js';
 import chainedDeps from './dependent-services/_.js';
 
 import { category } from '../utils.js';
@@ -6,5 +7,5 @@ import { category } from '../utils.js';
 export default category({
   output: 'di/comparisons',
   description: 'Compare **udic** with other DI libraries.',
-  list: [oneDep, chainedDeps],
+  list: [oneDep, chainedDeps, multiDep],
 });
