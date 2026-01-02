576c2ca0ee4bb89872bff6f29153ca08cb960d67
aquapi <aquaplmc@gmail.com>
2026-01-03 00:18:15 +0700

    update

--- snippets/concurrency/semaphore/p-limit.ts
+++ snippets/concurrency/semaphore/p-limit.ts
@@ -1,5 +1,5 @@
 import { readFile, hash } from './file.ts';
-import pLimit from 'p-limit';
+import { limitFunction } from 'p-limit';
 
 const getHash = async (path: string) => hash(await readFile(path));
 
@@ -7,8 +7,7 @@ export const getHashes = (paths: string[]) => {
   // No limit when not needed
   if (paths.length < 10) return Promise.all(paths.map(getHash));
 
-  const limit = pLimit(10);
   return Promise.all(
-    paths.map((path) => limit(getHash, path))
+    paths.map(limitFunction(getHash, { concurrency: 10 }))
   );
 }
